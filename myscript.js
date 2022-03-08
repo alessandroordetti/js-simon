40a003de9f51aa755a17e3441c1f38907485e93e
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-08 13:35:43 +0100

    Random numbers generated

--- myscript.js
+++ myscript.js
@@ -5,3 +5,11 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
+let randomNumbers = document.getElementById('random-numbers');
+
+/* Faccio comparire a schermo 5 numeri casuali */
+for (let i = 0; i < 5; i++){
+    randomNumbers.innerHTML += Math.floor(Math.random() * 50) + " ";
+}
+
+console.log(randomNumbers);
